4988ba3a7832d77c2476d875f3a6fbd68a9f4a71
Fanpeng Li <serforfp@gmail.com>
2024-01-23 17:23:12 +0000

    Change node to ES6 syntax, Add apollo server pkg

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const { createHandler } = require("graphql-http/lib/use/express");
 const port = process.env.PORT || 5000;
 
 const app = express();
